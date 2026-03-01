cd02d81b5e0ca585b9288bf2c25f343bc9c5a171
twaitsy <twaitsy@hotmail.co.uk>
2026-03-01 01:25:22 +0000

    Implement parent-aware FSM transition lifecycle calls

--- Assets/FSM/UnitBrain.cs
+++ Assets/FSM/UnitBrain.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class UnitBrain : MonoBehaviour
 {
@@ -46,11 +47,51 @@ public class UnitBrain : MonoBehaviour
         {
             if (t.From == current && t.EventName == evt)
             {
-                current.OnExit(this);
-                current = t.To;
-                current.OnEnter(this);
+                TransitionTo(t.To);
                 return;
             }
         }
+    }
+
+    private static List<BehaviourState> BuildAncestryChain(BehaviourState state)
+    {
+        List<BehaviourState> chain = new List<BehaviourState>();
+
+        while (state != null)
+        {
+            chain.Add(state);
+            state = state.Parent;
+        }
+
+        return chain;
+    }
+
+    private void TransitionTo(BehaviourState target)
+    {
+        if (target == null)
+            throw new ArgumentNullException(nameof(target));
+
+        if (current == target)
+            return;
+
+        List<BehaviourState> oldChain = BuildAncestryChain(current);
+        List<BehaviourState> newChain = BuildAncestryChain(target);
+
+        int oldIndex = oldChain.Count - 1;
+        int newIndex = newChain.Count - 1;
+
+        while (oldIndex >= 0 && newIndex >= 0 && oldChain[oldIndex] == newChain[newIndex])
+        {
+            oldIndex--;
+            newIndex--;
+        }
+
+        for (int i = 0; i <= oldIndex; i++)
+            oldChain[i].OnExit(this);
+
+        current = target;
+
+        for (int i = newIndex; i >= 0; i--)
+            newChain[i].OnEnter(this);
     }
 }
